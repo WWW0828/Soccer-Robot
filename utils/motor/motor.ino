1cc2ea8230a2d1e332c1d13fca3a1c1bbae0432f
108000232 <jmyapple9@gmail.com>
2023-05-19 19:10:00 +0800

    car motor test

--- utils/motor/motor.ino
+++ utils/motor/motor.ino
@@ -1,65 +1,104 @@
-int motor_left_1 = 10, motor_left_2 = 9;
-int motor_right_1 = 6, motor_right_2 = 5;
-int speed = 200;
+const int speedPinA = 5;  // Speed control pin for Motor A
+const int speedPinB = 10;  // Speed control pin for Motor B
+const int dirPinA1 = 6;    // Direction control pin 1 for Motor A
+const int dirPinA2 = 7;    // Direction control pin 2 for Motor A
+const int dirPinB1 = 8;    // Direction control pin 1 for Motor B
+const int dirPinB2 = 9;    // Direction control pin 2 for Motor B
+const int speed = 130;     // Speed of two motors, min:0, max: 255, but value exceed 200 would be the same as 200
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(motor_left_1, OUTPUT);
-  pinMode(motor_left_2, OUTPUT);
-  pinMode(motor_right_1, OUTPUT);
-  pinMode(motor_right_2, OUTPUT);
-  Serial.begin(9600);
+void forward() {
+  
+  analogWrite(speedPinA, speed);
+  analogWrite(speedPinB, speed);
+  
+  digitalWrite(dirPinA1,HIGH);
+  digitalWrite(dirPinA2,LOW);
+  digitalWrite(dirPinB1,HIGH);
+  digitalWrite(dirPinB2,LOW);
 }
 
-void turnLeft(int speed) {
-  Serial.println("turn left");
-  analogWrite(motor_left_1, 0);
-  analogWrite(motor_left_2, 0);
-  analogWrite(motor_right_1, speed);
-  analogWrite(motor_right_2, 0);
+void backward() {
+  analogWrite(speedPinA, speed);
+  analogWrite(speedPinB, speed);
+
+  digitalWrite(dirPinA1,LOW);
+  digitalWrite(dirPinA2,HIGH);
+  digitalWrite(dirPinB1,LOW);
+  digitalWrite(dirPinB2,HIGH);
 }
 
-void turnRight(int speed) {
-  Serial.println("turn right");
-  analogWrite(motor_left_1, speed);
-  analogWrite(motor_left_2, 0);
-  analogWrite(motor_right_1, 0);
-  analogWrite(motor_right_2, 0);
+void turnRight() {
+
+  analogWrite(speedPinA, speed);
+  analogWrite(speedPinB, 0);
+  
+  digitalWrite(dirPinA1,HIGH);
+  digitalWrite(dirPinA2,LOW);
+  digitalWrite(dirPinB1,LOW);
+  digitalWrite(dirPinB2,LOW);
 }
 
-void Forward(int speed) {
-  Serial.println("forward");
-  analogWrite(motor_left_1, speed);
-  analogWrite(motor_left_2, 0);
-  analogWrite(motor_right_1, speed);
-  analogWrite(motor_right_2, 0);
-}
+void turnLeft() {
+  
+  analogWrite(speedPinA, 0);
+  analogWrite(speedPinB, speed);
 
-void Backward(int speed) {
-  Serial.println("backward");
-  analogWrite(motor_left_1, 0);
-  analogWrite(motor_left_2, speed);
-  analogWrite(motor_right_1, 0);
-  analogWrite(motor_right_2, speed);
-}
+  digitalWrite(dirPinA1,LOW);
+  digitalWrite(dirPinA2,LOW);
+  digitalWrite(dirPinB1,HIGH);
+  digitalWrite(dirPinB2,LOW);
+} 
+void circle() { 
+  
+  analogWrite(speedPinA, speed);
+  analogWrite(speedPinB, speed);
 
-void Stop() {
-  Serial.println("stop");
-  analogWrite(motor_left_1, 0);
-  analogWrite(motor_left_2, 0);
-  analogWrite(motor_right_1, 0);
-  analogWrite(motor_right_2, 0);
-}
+  digitalWrite(dirPinA1,HIGH);
+  digitalWrite(dirPinA2,LOW);
+  digitalWrite(dirPinB1,LOW);
+  digitalWrite(dirPinB2,HIGH);
+}  
+ 
+void stop() {
 
-void loop() {
-  turnLeft(speed);
-  delay(2000);
-  turnRight(speed);
-  delay(2000);
-  Forward(speed);
-  delay(2000);
-  Backward(speed);
-  delay(2000);
-  Stop();
-  delay(2000);
+  analogWrite(speedPinA, 0);
+  analogWrite(speedPinB, 0);
+  
+  digitalWrite(dirPinA1,LOW);
+  digitalWrite(dirPinA2,LOW);
+  digitalWrite(dirPinB1,LOW);
+  digitalWrite(dirPinB2,LOW);
+} 
+ 
+void setup() 
+{ 
+  pinMode(speedPinA, OUTPUT);
+  pinMode(speedPinB, OUTPUT);
+  pinMode(dirPinA1, OUTPUT);
+  pinMode(dirPinA2, OUTPUT);
+  pinMode(dirPinB1, OUTPUT);
+  pinMode(dirPinB2, OUTPUT);
+}
+void loop()
+{
+    forward();
+    delay(2000);
+    stop();
+    delay(1000);
+    backward();
+    delay(2000);
+    stop();
+    delay(1000);
+    turnRight();
+    delay(2000);
+    stop();
+    delay(1000);
+    turnLeft();
+    delay(2000);
+    stop();
+    delay(1000);
+    circle();
+    delay(2000);
+    stop();
+    delay(1000);
 }
